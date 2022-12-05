ccc9188e60e6b644d230d9678405350af73f5d46
Fasero11 <86266311+Fasero11@users.noreply.github.com>
2022-12-05 13:47:48 +0100

    Changed Thread_Controller for Arduino_FreeRTOS

--- sketch_followline/sketch_followline.ino
+++ sketch_followline/sketch_followline.ino
@@ -1,6 +1,7 @@
 #include <StaticThreadController.h>
 #include <Thread.h>
 #include <ThreadController.h>
+#include <Arduino_FreeRTOS.h>
 
 #define IR_THRESHOLD 100
 
@@ -8,7 +9,7 @@
 #define TRIG_PIN 13  
 #define ECHO_PIN 12 
 
-// infrared sensor ( de 0 a 1024 ) analog write
+// infrared sensor ( from 0 to 1024 ) analog write
 #define PIN_ITR20001_LEFT   A2
 #define PIN_ITR20001_MIDDLE A1
 #define PIN_ITR20001_RIGHT  A0
@@ -32,14 +33,6 @@
 
 //ultrasonic sensor 
 #define MAX_DIST_OBSTACLE 8
-//#define MIN_DIST_OBSTACLE 0
-
-// in milliseconds
-#define THREE_HUNDRED_MS 300
-
-
-//in microseconds
-#define TEN_MS 10
 
 #define START_LAP 1
 #define END_LAP 2
@@ -57,13 +50,57 @@ int middle_ir;
 int right_ir;
 int message;
 
-ThreadController controller = ThreadController();
-Thread distanceThread = Thread();
-Thread IRThread = Thread();
-Thread msgThread = Thread();
-
 void send_message(){
-    Serial.print(message);    
+  TickType_t xLastWaskeTime, aux;
+  while(1){
+      xLastWaskeTime = xTaskGetTickCount();
+
+      message = 0;
+      if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
+        message = LINE_LOST;
+      } else if(detected_obstacle){
+        message = OBSTACLE_DETECTED;
+      }
+      
+      Serial.print(message);
+      
+      xTaskDelayUntil(&xLastWaskeTime, 20);
+    }    
+}
+
+void get_infrared(){
+  TickType_t xLastWaskeTime, aux;
+  while(1){
+      xLastWaskeTime = xTaskGetTickCount();
+      left_ir = analogRead(PIN_ITR20001_LEFT);
+      middle_ir = analogRead(PIN_ITR20001_MIDDLE);
+      right_ir = analogRead(PIN_ITR20001_RIGHT);
+        
+//      Serial.print("LEFT: ");
+//      Serial.print(left_ir);
+//      Serial.print(" MIDDLE: ");
+//      Serial.print(middle_ir);
+//      Serial.print(" RIGHT: ");
+//      Serial.println(right_ir);
+      
+      xTaskDelayUntil(&xLastWaskeTime, 10);
+  }    
+}
+
+void is_obstacle(){
+  TickType_t xLastWaskeTime, aux;
+  while(1){
+      xLastWaskeTime = xTaskGetTickCount();
+      int distance_sensor = get_distance();
+    
+      //Serial.println(distance_sensor);
+      if (distance_sensor < MAX_DIST_OBSTACLE){
+        detected_obstacle = true;
+      } else {
+        detected_obstacle = false;  
+      }
+      xTaskDelayUntil(&xLastWaskeTime, 10);
+  }
 }
 
 //return distance in cm
@@ -78,32 +115,9 @@ int get_distance(){
   
   // conversion into cm 
   distance = time / 29 / 2; 
-  // Serial.println(distance);
+  //Serial.println(distance);
 
   return distance;
-}
-
-void get_infrared(){
-  left_ir = analogRead(PIN_ITR20001_LEFT);
-  middle_ir = analogRead(PIN_ITR20001_MIDDLE);
-  right_ir = analogRead(PIN_ITR20001_RIGHT);
-    
-//  Serial.print("LEFT: ");
-//  Serial.print(left_ir);
-//  Serial.print(" MIDDLE: ");
-//  Serial.print(middle_ir);
-//  Serial.print(" RIGHT: ");
-//  Serial.println(right_ir);    
-}
-
-void callback_dist_thread(){
-
-  int distance_sensor = get_distance();
-
-  //Serial.println(distance_sensor);
-  if (distance_sensor < MAX_DIST_OBSTACLE){
-    detected_obstacle = true;
-  }
 }
 
 void setup() {
@@ -112,41 +126,13 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
-  //THREAD SETUP
-  distanceThread.enabled = true;
-  distanceThread.setInterval(300); // ms
-  distanceThread.onRun(callback_dist_thread);
-
-  IRThread.enabled = true;
-  IRThread.setInterval(100); // ms
-  IRThread.onRun(get_infrared);
-
-  msgThread.enabled = true;
-  msgThread.setInterval(1000); // ms
-  msgThread.onRun(send_message);
-
-  controller.add(&distanceThread);
-  controller.add(&IRThread);
-  controller.add(&msgThread); 
+  xTaskCreate(is_obstacle, "is_obstacle", 300, NULL, 2, NULL);
+  xTaskCreate(get_infrared, "get_infrared", 100, NULL, 1, NULL);
+  xTaskCreate(send_message, "send_message", 500, NULL, 0, NULL);
 
   Serial.begin(9600);
 
 }
 
 void loop() {
-    
-  if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
-    message = LINE_LOST;
-  } else {
-    message = 0;
-  }
-    
-  if(detected_obstacle){
-    controller.remove(&distanceThread);
-    // Send message once every second.
-    message = OBSTACLE_DETECTED;
-    controller.add(&msgThread); 
-  }
-
-  controller.run();  
 }
