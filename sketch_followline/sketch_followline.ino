939e0a0e81d7de691093cb6ccc45344554cb3738
jlopeza2020 <90765012+jlopeza2020@users.noreply.github.com>
2022-12-11 21:44:50 +0100

    follows straight lines

--- sketch_followline/sketch_followline.ino
+++ sketch_followline/sketch_followline.ino
@@ -59,9 +59,6 @@ long aux_time = 0;
 
 int left_ir, middle_ir, right_ir, message, prev_time, line, count;
 
-//int smooth_right;
-//int smooth_left;
-
 void send_message(){
   TickType_t xLastWaskeTime, aux;
 
@@ -79,7 +76,7 @@ void send_message(){
         message = PING;
       }
 
-      //Serial.print(message);
+      Serial.print(message);
 
       xTaskDelayUntil(&xLastWaskeTime, 20);
     }    
@@ -88,62 +85,43 @@ void send_message(){
 void get_infrared(){
   TickType_t xLastWaskeTime, aux;
   while(1){
-      xLastWaskeTime = xTaskGetTickCount();
-      left_ir = analogRead(PIN_ITR20001_LEFT);
-      middle_ir = analogRead(PIN_ITR20001_MIDDLE);
-      right_ir = analogRead(PIN_ITR20001_RIGHT);
-
-      Serial.println(left_ir);
-
-      // MEJORAR VALORES PARA QUE GIRE MEJOR
-      // ESTE CASO NO MERECE LA PENA INCLUIRLO, LA CINTA NUNCA TAPA LOS 3 SENSORES 
-      /*if (left_ir < MAX_IR_THRESHOLD && middle_ir < MAX_IR_THRESHOLD && right_ir < MAX_IR_THRESHOLD &&
-          left_ir > MIN_IR_THRESHOLD && middle_ir > MIN_IR_THRESHOLD && right_ir < MIN_IR_THRESHOLD ){
-        line = 0;
-      } */
-
-      // TODOS ESTÁN FUERA 
-      if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
-        is_line = false;
-        line = NO_LINE;
-
-      }
-      // AÑADIR EL CASO DE QUE SOLO TOQUE AL DEL CENTRO Y SERÁ LINE == 0 
-
-      // CENTRO DERECHA
-      else if (left_ir < IR_THRESHOLD && middle_ir >= IR_THRESHOLD && right_ir >= IR_THRESHOLD){
-        //Serial.println("muy der");
-        line = LINE_MIDRIGHT;
-      }
-      // DERECHA
-      else if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir >= IR_THRESHOLD){
-        //Serial.println("der");
-        line = LINE_RIGHT;
-      }
-      // CENTRO IZQUIERDA
-      else if (left_ir >= IR_THRESHOLD && middle_ir >= IR_THRESHOLD && right_ir < IR_THRESHOLD){
-        //Serial.println("muy izq");
-        line = LINE_MIDLEFT;
-      }
-      // IZQUIERDA
-      else if (left_ir >= IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
-        //Serial.println("izq");
-        line = LINE_LEFT;
-      }
-      // CENTRO
-      else {
-        line = LINE_MID;
-      }
+    xLastWaskeTime = xTaskGetTickCount();
+    left_ir = analogRead(PIN_ITR20001_LEFT);
+    middle_ir = analogRead(PIN_ITR20001_MIDDLE);
+    right_ir = analogRead(PIN_ITR20001_RIGHT);
 
 
-//      Serial.print("LEFT: ");
-//      Serial.print(left_ir);
-//      Serial.print(" MIDDLE: ");
-//      Serial.print(middle_ir);
-//      Serial.print(" RIGHT: ");
-//      Serial.println(right_ir);
+    // TODOS ESTÁN FUERA 
+    if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
+      is_line = false;
+      line = NO_LINE;
+    }
+    if (left_ir < IR_THRESHOLD && middle_ir >= IR_THRESHOLD && right_ir >= IR_THRESHOLD){
+      // RIGHT
+      line = LINE_RIGHT;
 
-      xTaskDelayUntil(&xLastWaskeTime, 10);
+    }
+    if (left_ir < IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir >= IR_THRESHOLD){
+      // CENTER AND RIGHT
+      line = LINE_MIDRIGHT;
+
+    }
+    if (left_ir >= IR_THRESHOLD && middle_ir >= IR_THRESHOLD && right_ir < IR_THRESHOLD){
+      // LEFT
+      line = LINE_LEFT;
+
+    }
+    if (left_ir >= IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir < IR_THRESHOLD){
+      // CENTER AND LEFT
+      line = LINE_MIDLEFT;
+
+    } 
+    if(left_ir >= IR_THRESHOLD && middle_ir < IR_THRESHOLD && right_ir >= IR_THRESHOLD){
+      // JUST THE MIDDLE TOUCH THE LINE
+      line = LINE_MID;
+    }
+    
+    xTaskDelayUntil(&xLastWaskeTime, 10);
   }    
 }
 
@@ -167,13 +145,13 @@ int get_distance(){
 void is_obstacle(){
   TickType_t xLastWaskeTime, aux;
   while(1){
-    //Serial.println("CHECKING ULTRASOUND");
+
     xLastWaskeTime = xTaskGetTickCount();
     int distance_sensor = get_distance();
 
-    //Serial.println(distance_sensor);
     if (distance_sensor < MAX_DIST_OBSTACLE){
       detected_obstacle = true;
+      //Serial.println("obstacle");
     } else {
       detected_obstacle = false;  
     }
@@ -184,7 +162,7 @@ void is_obstacle(){
 void command_motors(){
   TickType_t xLastWaskeTime, aux;
   while(1){
-      //Serial.println("COMMANDING MOTORS");
+
       xLastWaskeTime = xTaskGetTickCount();
       // set high each time we use them 
       digitalWrite(PIN_Motor_AIN_1, HIGH);
@@ -192,9 +170,9 @@ void command_motors(){
 
       if (line == LINE_MID && !detected_obstacle){
         // right 
-        analogWrite(PIN_Motor_PWMA, 40);
+        analogWrite(PIN_Motor_PWMA, 50);
         // left
-        analogWrite(PIN_Motor_PWMB, 40); 
+        analogWrite(PIN_Motor_PWMB, 50); 
 
       }
       if (line == NO_LINE || detected_obstacle){
@@ -203,37 +181,35 @@ void command_motors(){
         analogWrite(PIN_Motor_PWMB, 0);     
       }
 
-      if (line == LINE_RIGHT && !detected_obstacle){
-        // right 
-        analogWrite(PIN_Motor_PWMA, 40);
-        // left
-        analogWrite(PIN_Motor_PWMB, 20); 
-
-      }
-      if (line == LINE_MIDRIGHT && !detected_obstacle){
-        // right 
-        analogWrite(PIN_Motor_PWMA, 30);
-        // left
-        analogWrite(PIN_Motor_PWMB, 0);
-
-      }
-      if (line == LINE_LEFT && !detected_obstacle){
+      if (line == LINE_MIDRIGHT && !detected_obstacle){ // gira izq + mid 
         // right 
         analogWrite(PIN_Motor_PWMA, 20);
+        // left
+        analogWrite(PIN_Motor_PWMB, 40); 
+
+      }
+      if (line == LINE_RIGHT && !detected_obstacle){ // gira izq
+        // right 
+        analogWrite(PIN_Motor_PWMA, 0);
         // left
         analogWrite(PIN_Motor_PWMB, 40);
 
       }
-      //Serial.println(smooth_left);
-      if (line == LINE_MIDLEFT && !detected_obstacle){
-
+      if (line == LINE_MIDLEFT && !detected_obstacle){ //  gira derecha + mid
         // right 
-        analogWrite(PIN_Motor_PWMA, 0);
+        analogWrite(PIN_Motor_PWMA, 40);
         // left
-        analogWrite(PIN_Motor_PWMB, 30);
+        analogWrite(PIN_Motor_PWMB, 20);
 
       }
+      if (line == LINE_LEFT && !detected_obstacle){ // gira derecha
 
+        // right 
+        analogWrite(PIN_Motor_PWMA, 40);
+        // left
+        analogWrite(PIN_Motor_PWMB, 0);
+
+      }
       xTaskDelayUntil(&xLastWaskeTime, 10);
   }   
 }
@@ -274,10 +250,10 @@ void setup() {
 
   // communicate arduino with ESP to start lap
 
-  xTaskCreate(is_obstacle, "is_obstacle", 512, NULL, 3, NULL);
-  xTaskCreate(get_infrared, "get_infrared", 512, NULL, 1, NULL);
-  xTaskCreate(send_message, "send_message", 512, NULL, 0, NULL);
-  xTaskCreate(command_motors, "command_motors", 512, NULL, 4, NULL);
+  xTaskCreate(is_obstacle, "is_obstacle", 100, NULL, 3, NULL);
+  xTaskCreate(get_infrared, "get_infrared", 100, NULL, 1, NULL);
+  xTaskCreate(send_message, "send_message", 100, NULL, 0, NULL);
+  xTaskCreate(command_motors, "command_motors", 100, NULL, 4, NULL);
 
   // state message sent each 4 seconds, time field should represent time
   // since lap started . Should prioritize follow line behaviour to 
@@ -285,8 +261,19 @@ void setup() {
   //xTaskCreate(send_ping, "send_ping", 512, NULL, 2, NULL);
 
   Serial.begin(9600);
+  //Serial.setTimeout(50);
 
 }
 
 void loop() {
+
+  // intento de comunicación del esp al arduino 
+  /*if (Serial.available())
+  {
+      char data = Serial.read();
+      if (data == 'C'){
+        
+        Serial.println(data);
+      }
+  }*/
 }
